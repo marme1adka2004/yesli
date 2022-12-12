6b7b4ec891644134e16a6c838f588ef9b1ce9c7e
marme1adka2004 <gaisma2000@gmail.com>
2022-12-12 14:24:01 +0600

    first commit

---
+++ src/js/files/functions.js
@@ -0,0 +1,106 @@
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+export function isWebp() {
+	// Проверка поддержки webp
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	// Добавление класса _webp или _no-webp для HTML
+	testWebP(function (support) {
+		let className = support === true ? 'webp' : 'no-webp';
+		document.documentElement.classList.add(className);
+	});
+}
+/* Проверка мобильного браузера */
+export let isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+/* Добавление класса touch для HTML если браузер мобильный */
+export function addTouchClass() {
+	// Добавление класса _touch для HTML если браузер мобильный
+	if (isMobile.any()) document.documentElement.classList.add('touch');
+}
+// Добавление loaded для HTML после полной загрузки страницы
+export function addLoadedClass() {
+	window.addEventListener("load", function () {
+		setTimeout(function () {
+			document.documentElement.classList.add('loaded');
+		}, 0);
+	});
+}
+// Получение хеша в адресе сайта
+export function getHash() {
+	if (location.hash) { return location.hash.replace('#', ''); }
+}
+// Указание хеша в адресе сайта
+export function setHash(hash) {
+	history.pushState('', '', hash);
+}
+
+
+
+//==== Вспомогательные модули блокировки прокрутки и скочка ====================================================================================================================================================================================================================================================================================
+export let bodyLockStatus = true;
+export let bodyLockToggle = (delay = 500) => {
+	if (document.documentElement.classList.contains('lock')) {
+		bodyUnlock(delay);
+	} else {
+		bodyLock(delay);
+	}
+}
+export let bodyUnlock = (delay = 500) => {
+	let body = document.querySelector("body");
+	if (bodyLockStatus) {
+		let lock_padding = document.querySelectorAll("._lp");
+		setTimeout(() => {
+			for (let index = 0; index < lock_padding.length; index++) {
+				const el = lock_padding[index];
+				el.style.paddingRight = '0px';
+			}
+			body.style.paddingRight = '0px';
+			document.documentElement.classList.remove("lock");
+		}, delay);
+		bodyLockStatus = false;
+		setTimeout(function () {
+			bodyLockStatus = true;
+		}, delay);
+	}
+}
+export let bodyLock = (delay = 500) => {
+	let body = document.querySelector("body");
+	if (bodyLockStatus) {
+		let lock_padding = document.querySelectorAll("._lp");
+		for (let index = 0; index < lock_padding.length; index++) {
+			const el = lock_padding[index];
+			el.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+		}
+		body.style.paddingRight = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+		document.documentElement.classList.add("lock");
+
+		bodyLockStatus = false;
+		setTimeout(function () {
+			bodyLockStatus = true;
+		}, delay);
+	}
+}
+
+
+//==== Модуь работы с меню (бургер) =======================================================================================================================================================================================================================
+export function menuOpen() {
+	let iconMenu = document.querySelector(".icon-menu");
+	if (iconMenu) {
+		let menu = document.querySelector(".menu");
+		iconMenu.addEventListener("click", function (e) {
+			if (bodyLockStatus) {
+				bodyLockToggle();
+				menu.classList.toggle("menu-open");
+			}
+		});
+	};
+}
+export function menuClose() {
+	let menu = document.querySelector(".menu");
+	if (menu) menu.classList.remove("menu-open");
+}
+
---
+++ src/js/files/script.js
@@ -0,0 +1,6 @@
+// Проверка мобильного браузера
+// import { isMobile } from "./functions";
+$(function(){
+    $("#phone").mask("+7(999) 999-99-99");
+  });
+  
